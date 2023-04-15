c155eff073bdc34f407f298e118d8fbac8fce775
falarion08 <barrion.ryanonil@gmail.com>
2023-04-15 16:16:33 +0800

    Update CSOPESY- S11-Project2.py

--- CSOPESY- S11-Project2.py
+++ CSOPESY- S11-Project2.py
@@ -2,6 +2,18 @@ import threading
 from threading import *
 from numpy.random import *
 
+z = None
+threads_inside = 0
+
+class threadID:
+    def __init__(self, id, color):
+        self.id = int(id)
+        self.color = color
+        
+        
+
+
+
 def main():
     n = int(input('Enter the number of slots in the fitting room:'))
     b = int(input('Enter the number of blue threads:'))
@@ -9,31 +21,13 @@ def main():
     
     maxSlots = BoundedSemaphore(n)
 
-    blue_threads = Semaphore()
-    green_threads = Semaphore() 
+    semaphore = Semaphore()
     
     # [0] Blue First, [1] Green First
     chooser = randint(2)
     
-    if (b and chooser == 0) or (g <= 0 and chooser):
-        firstColor = "Blue"
-        bCnt = n
-        gCnt = 0
+    # if (chooser == 0 and b > 0) or (chooser == 1 and b < 0):
         
-        blue_ready = [True for i in range(b)]
-        green_ready = [False for i in range(g)]
-        
-    elif (g and chooser) or (b <= 0 and chooser == 0):
-        firstColor = 'Green'
-        bCnt = 0
-        gCnt = n
-
-        blue_ready = [False for i in range(b)]
-        green_ready  = [True for i in range(g)]
-        
-        
-    
-                
 
 if __name__ == '__main__':
     main()
