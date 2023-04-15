f37be4006ff915dc85d39504af50b9281ad2040d
falarion08 <barrion.ryanonil@gmail.com>
2023-04-15 16:28:41 +0800

    Update CSOPESY- S11-Project2.py

--- CSOPESY- S11-Project2.py
+++ CSOPESY- S11-Project2.py
@@ -4,30 +4,31 @@ from numpy.random import *
 
 z = None
 threads_inside = 0
+maxSlots = None
 
 class threadID:
     def __init__(self, id, color):
         self.id = int(id)
         self.color = color
-        
-        
+        thread = Thread(target=dressingRoom, args = [id,color])
 
+def dressingRoom(thread_id, thread_color):
+    maxSlots.acquire()
+    print(f'ID: {thread_id} {thread_color} has entered the fitting room')
+    maxSlots.release()
+     
 
+n = int(input('Enter the number of slots in the fitting room:'))
+b = int(input('Enter the number of blue threads:'))
+g = int(input('Enter the number of green threads:'))
 
-def main():
-    n = int(input('Enter the number of slots in the fitting room:'))
-    b = int(input('Enter the number of blue threads:'))
-    g = int(input('Enter the number of green threads:'))
+maxSlots = BoundedSemaphore(n)
+
+semaphore = Semaphore()
+
+# [0] Blue First, [1] Green First
+chooser = randint(2)
+
+if (chooser == 0 and b > 0) or (chooser == 1 and b < 0):
+    print('hey')
     
-    maxSlots = BoundedSemaphore(n)
-
-    semaphore = Semaphore()
-    
-    # [0] Blue First, [1] Green First
-    chooser = randint(2)
-    
-    # if (chooser == 0 and b > 0) or (chooser == 1 and b < 0):
-        
-
-if __name__ == '__main__':
-    main()
